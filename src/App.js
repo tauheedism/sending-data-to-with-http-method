d3bb59a7a3c9a32eb171bc2337da6988037d32ea
Tauheed_Siddiqui <tauheedsiddiqui8760@gmail.com>
2023-07-10 19:34:01 +0530

    Adding DateFormatter and Loader

--- src/App.js
+++ src/App.js
@@ -2,20 +2,30 @@ import React, { Fragment, useState } from "react";
 import MovieList from "./components/MovieList";
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [isLoader,setIsLoader]=useState(false)
 
   const fetchMoviesHandler = async () => {
+    setIsLoader(true)
     const response = await fetch("https://swapi.py4e.com/api/films/");
     const data = await response.json();
     const transformedMovies = data.results.map((movieData) => {
+      var options = { year: 'numeric', month: 'long', day: 'numeric' };
+      const formattedDate = new Date(movieData.release_date).toLocaleDateString([],options);
       return {
         id: movieData.episode_id,
         title: movieData.title,
         openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_data,
+        releaseDate: formattedDate,
+        director:movieData.director,
       };
     });
     setMovies(transformedMovies);
+    setIsLoader(false)
+  console.log(data.results);
   };
+
+
+  
 
   return (
     <Fragment>
@@ -23,7 +33,7 @@ const App = () => {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MovieList movies={movies} />
+        {isLoader?'loading.....':<MovieList movies={movies} />}
       </section>
     </Fragment>
   );
